7e2a421d4f87136b1817331fb4ae4210d011dc65
Reform Digital® <github@reform.digital>
2022-08-18 15:32:33 +0300

    Completed coding challenge 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,17 @@
-let js = `amazing`;
-console.log(js);
+// const markMass = 78;
+// const markHeight = 1.69;
+// const johnMass = 92;
+// const johnHeight = 1.95;
+
+const markMass = 95;
+const markHeight = 1.88;
+const johnMass = 85;
+const johnHeight = 1.76;
+
+const markBMI = markMass / markHeight ** 2;
+const johnBMI = johnMass / johnHeight ** 2;
+
+const markHigherBMI = markBMI > johnBMI;
+console.log(`Mark's BMI is ${markBMI}`);
+console.log(`John's BMI is ${johnBMI}`);
+console.log(`It is ${markHigherBMI} that Mark has a higher BMI than John`);
